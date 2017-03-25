d39743f4d6356a6fa0a14b89723ff6ab9a7e421c
Nemzy <nemsytomic@gmail.com>
2017-03-25 08:23:42 +0100

    Data processing - changed

--- data_utils.py
+++ data_utils.py
@@ -29,20 +29,23 @@ def get_data_from_article(file_path):
 
 		contents.append(content)
 
-def make_data_set():
-	vocab_dict = Counter(w for txt in titles+contents for w in txt.split())
+def make_data_set(drop_n = 0):
+	vocab_dict = Counter(w.strip(',."\' ;:)(?!') for txt in titles+contents for w in txt.split())
 	vocab = map(lambda x: x[0], sorted(vocab_dict.items(), key = lambda x: -x[1]))
-	
+
+	if drop_n > 0:
+		vocab = vocab[:-drop_n]
+
 	word2idx = dict([(word, idx+2) for idx, word in enumerate(vocab)])
-	word2idx['<empty>'] = 0
+	word2idx['<ukn>'] = 0
 	word2idx['<eos>'] = 1
 
 	idx2word = dict([(idx, word) for word, idx in word2idx.iteritems()])
 
-	Y = [[word2idx[word] for word in title.split()] for title in titles]
-	X = [[word2idx[word] for word in content.split()] for content in contents]
+	Y = [[word2idx.get(word.strip(',."\' ;:)(?!'), 0) for word in title.split()] for title in titles]
+	X = [[word2idx.get(word.strip(',."\' ;:)(?!'), 0) for word in content.split()] for content in contents]
 
-	return X, Y, word2idx, idx2word, vocab, vocab_dict
+	return X, Y, word2idx, idx2word, vocab
 
 def save_data_set(filename, obj):
 	with open(filename, 'wb') as f:
@@ -55,10 +58,9 @@ def read_data_set(filename):
 
 if __name__ == '__main__':
 
-	walk = os.walk('data/')
+	walk = os.walk('../data/')
 	next(walk)
 	for dirpath, dirnames, filenames in walk:
 		for filename in filenames:
 			get_data_from_article(os.path.join(dirpath, filename))
-
 	save_data_set(filename = 'data.pkl', obj = make_data_set())
